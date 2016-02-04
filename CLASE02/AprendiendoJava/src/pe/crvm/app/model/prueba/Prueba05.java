f3d8d87a9bb1023d3c68aac258cf407d67c63c82
CarlosVilchezMalasquez <1310374@utp.edu.pe>
2016-02-04 13:49:38 -0500

    Actualizando Datos

---
+++ CLASE02/AprendiendoJava/src/pe/crvm/app/model/prueba/Prueba05.java
@@ -0,0 +1,16 @@
+
+package pe.crvm.app.model.prueba;
+
+import pe.crvm.app.model.service.FacturaService;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Prueba05 {
+    public static void main(String[] args) {
+        System.out.println("Mayor de 12 y 18: "+Math.max(12, 18));
+        
+        System.out.println("IGV: "+FacturaService.IGV);
+    }
+}
---
+++ CLASE02/AprendiendoJava/src/pe/crvm/app/model/service/FacturaService.java
@@ -0,0 +1,16 @@
+
+package pe.crvm.app.model.service;
+
+/**
+ *
+ * @author Alumno
+ */
+public class FacturaService {
+    
+    public static double IGV;
+    
+    /* Permite inicializar valores estaticas */
+    static{
+        IGV = 0.18;
+    }
+}
