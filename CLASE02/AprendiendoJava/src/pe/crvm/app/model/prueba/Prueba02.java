024d61768614586b6a6af8243bb8a9bca6fb00ff
CarlosVilchezMalasquez <1310374@utp.edu.pe>
2016-02-04 09:52:13 -0500

    Actualizando Datos

---
+++ CLASE02/AprendiendoJava/src/pe/crvm/app/model/prueba/Prueba02.java
@@ -0,0 +1,17 @@
+
+package pe.crvm.app.model.prueba;
+
+import com.oracle.jrockit.jfr.Producer;
+import pe.crvm.app.model.Producto;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Prueba02 {
+    public static void main(String[] args) {
+        while(true){
+            new Producto();
+        }
+    }
+}
---
+++ CLASE02/PagoApp/src/pe/crvm/pagoapp/ClasePrincipal.java
@@ -0,0 +1,17 @@
+
+package pe.crvm.pagoapp;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ClasePrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
